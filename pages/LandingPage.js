a57cbf7807a77f99113aabf83fb3e95fdda356ba
JUWONCALEB <63285430+juwoncaleb@users.noreply.github.com>
2025-10-17 18:47:56 +0100

    send

--- pages/LandingPage.js
+++ pages/LandingPage.js
@@ -100,7 +100,7 @@ export default function LandingPage() {
           <div className="call mt-20 mb-20">
             <p className="text-white click mt-4 mb-4">Check Availability</p>
             <button className="install mb-6">
-              Yes! I'd like a Calendar Full of Appointments{" "}
+              Yes! I want a Calendar Full of Appointments{" "}
             </button>
           </div>
         </center>
